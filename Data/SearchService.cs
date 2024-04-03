8d3dfec93bf92809ec948466d612e526fb0519fb
Cryosys <s.sloot@web.de>
2024-04-03 22:36:51 +0200

    Added version link to release in nav bar

--- Data/SearchService.cs
+++ Data/SearchService.cs
@@ -2,6 +2,8 @@ namespace SearchCacher.Data
 {
 	internal class SearchService
 	{
+		public const string Version = "1.0.3.1";
+
 		static List<WeakReference<Action<string>>> currentSearchDirChangedHandlers = new List<WeakReference<Action<string>>>();
 		static object handlerLock = new object();
 
