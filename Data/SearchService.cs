4f5c817de847892522c57a51edf632ab801f7108
Cryosys <s.sloot@web.de>
2026-02-25 23:41:26 +0100

    Bumped version to 2.0.0.0

--- Data/SearchService.cs
+++ Data/SearchService.cs
@@ -4,7 +4,7 @@ namespace SearchCacher.Data
 {
 	internal interface ISearchService
 	{
-		public const string Version = "1.0.6.0";
+		public const string Version = "2.0.0.0";
 
 		public string SearchPath { get; }
 
